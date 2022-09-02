5ef690b450c62bebda105924ae0787ff64ae5d4d
pavan192110122 <112701830+pavan192110122@users.noreply.github.com>
2022-09-02 17:22:04 +0530

    Add files via upload

---
+++ reverse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+int n, reverse=0, rem;    
+cout<<"Enter a number: ";    
+cin>>n; 
+if(n==int(n)){
+  while(n!=0)    
+  {    
+     rem=n%10;      
+     reverse=reverse*10+rem;    
+     n/=10;    
+  }    
+ cout<<"Reversed Number: "<<reverse<<endl; 
+}
+else{
+	cout<<"invalid input";
+}
+return 0;  
+}  
